13a496fbe0f003b55cf0f538b46178f42839a765
MatteoZattini <matteozattini@hotmail.com>
2024-05-16 14:31:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      
+        
+    }
+  },
+
+  methods: {
+
+
+  },
+
+  mounted() {
+    axios.get('').then((result) => {
+      const element = result.data    
+    })
+  },
+  
+}).mount('#app')
